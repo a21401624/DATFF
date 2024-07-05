18d3fc2fed5f439168b47879c6864dd142cce66a
a21401624 <huyuxuan2021@ia.ac.cn>
2024-07-05 17:18:48 +0800

    fix bug

--- mmrotate/configs/bimodal_r3det_tiny/bimodal_r3det_tiny_r50_fpn_CCAT_UACMDet_config_DVFull_r+c_oc.py
+++ mmrotate/configs/bimodal_r3det_tiny/bimodal_r3det_tiny_r50_fpn_CCAT_UACMDet_config_DVFull_r+c_oc.py
@@ -133,7 +133,7 @@ model = dict(
         nms=dict(iou_thr=0.1),
         max_per_img=2000))
 # dataset settings
-dataset_type = 'BimodalDroneVehicleDataset'
+dataset_type = 'BimodalRDroneVehicleDataset'
 data_root = '/data2/likaihao/DroneVehicle/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
